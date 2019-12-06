190e697631619bb1ca353a1ce5dff59743f6ccec
Jered McInerney <jered.mcinerney@gmail.com>
2019-12-06 11:51:40 -0500

    bwh updates

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from unify_format.convert_bwh import main as unify_bwh
 from unify_format.convert_mimic_cxr import main as unify_mimic_cxr
 from assemble_dataset.reports_and_codes_dataset import main as make_reports_codes
 from assemble_dataset.reports_to_seq_dataset import main as make_reports_seq
@@ -5,7 +6,8 @@ from icd_codes.process_codes import main as process_codes
 from icd_codes.radiology_codes import main as radiology_codes
 
 if __name__ == '__main__':
-    make_reports_codes()
+    unify_bwh()
+#    make_reports_codes()
 #    radiology_codes()
 #    process_codes()
 #    make_reports_seq()
